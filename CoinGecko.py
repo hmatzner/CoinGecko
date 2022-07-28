4d932a9d134c238691c0bb16545f0379503ad034
Hernan Matzner <hernoo.m@gmail.com>
2022-07-28 19:59:06 +0300

    trying git

--- CoinGecko.py
+++ CoinGecko.py
@@ -3,8 +3,23 @@ from bs4 import BeautifulSoup
 from lxml import etree
 import pandas as pd
 from tqdm import tqdm
+import argparse
 
 COINGECKO_URL = 'https://www.coingecko.com'
+
+parser = argparse.ArgumentParser(description='Indicate how many coins, from 1 to 100, you would like to see.')
+parser.add_argument('-k', '--coins', type=int, metavar='', help='Number of coins')
+# parser.add_argument('coins', type=int, metavar='', help='Number of coins')
+
+
+# parser.add_argument('n1', type=float, metavar='', help='First nº of the calculation')
+# parser.add_argument('n2', type=float, metavar='',  help='Second nº of the calculation')
+
+# group = parser.add_mutually_exclusive_group()
+# group.add_argument('-w', '--morning', action='store_true', help='Gives a "Good morning" message')
+# group.add_argument('-n', '--night', action='store_true', help='Gives a "Good night" message')
+
+args = parser.parse_args()
 
 
 def get_soup(url):
@@ -47,7 +62,6 @@ def web_scraper(url, soup, k):
     """
     scraped_coins = soup.find_all('a', class_= "lg:tw-flex font-bold tw-items-center tw-justify-between")
     list_of_lists = list()
-
     print('Information being retrieved...')
 
     for coin in tqdm(scraped_coins[:k], total=k):
@@ -79,18 +93,28 @@ def web_scraper(url, soup, k):
 
 def main():
     """
-    Main function of the module:
-    - asks the user how many coins to get info about
-    - calls the other functions and prints a dataframe
+    Main function of the module that:
+    - assigns the operation to perform to a variable
+    - prints a message, if any
+    - prints the return value of the math operation to perform
     """
+    # function = dict_[args.operation]
+    # if args.morning:
+    #     print('Good morning!')
+    # elif args.night:
+    #     print('Good night!')
+    # print(function(args.n1, args.n2))
+    k = args.coins
+
+    if k is None:
+        print('ERROR: Please provide a value for k.')
+        return
+
+    if k not in range(1, 101):
+        print('ERROR: The value of k must be an integer from 1 to 100.')
+        return
+
     url, soup = get_soup(COINGECKO_URL)
-    while True:
-        k = input('Indicate how many coins, from 1 to 100, you would like to see: ')
-        if k in map(str, range(1, 101)):
-            k = int(k)
-            break
-        else:
-            print('The value must be an integer from 1 to 100.')
     df = web_scraper(url, soup, k)
     print(df)
 
