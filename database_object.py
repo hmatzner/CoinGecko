477fb5c8ff281205e830d6a0d06cd3325a24dd78
dorm12 <dor1213@gmail.com>
2022-08-16 18:04:04 +0300

    !

--- database_object.py
+++ database_object.py
@@ -1,19 +1,20 @@
 import pymysql
 import pandas as pd
-# import CoinGecko
 
 
 class Database:
     DB_NAME = 'crypto_currencies'
 
     def __init__(self, df=None, df_hist=None, db_name=DB_NAME):
-        # self.PASSWORD = input("provide password for mySQL server: ")
-        self.PASSWORD = 'kaBoom93'
+        try:
+            with open('.gitignore_folder\password.txt') as f:
+                self.PASSWORD = f.read()
+        except FileNotFoundError:
+            self.PASSWORD = input("provide password for mySQL server: ")
         self.db_name=db_name
-        self.cnx = None
-        self.cursor = None
         
-        self.create_connection(use_db=False)
+        self.connection = self.create_connection(use_db=False)
+        self.cursor = self.connection.cursor()
         self.create_database()
         
         self.cursor.execute("SHOW TABLES")
@@ -30,42 +31,40 @@ class Database:
         if df:
             self.append_rows_to_coins(df)
         if df_hist:
-            self.append_rows_to_coins(df_hist)
+            self.append_rows_to_history(df_hist)
 
     def create_connection(self, use_db=True):
         """creates and returns a connection and a cursor"""
         if use_db:
-            self.cnx = pymysql.connect(host='localhost',
-                                       user='root',
-                                       password=self.PASSWORD,
-                                       database=self.DB_NAME)
-            self.cursor = self.cnx.cursor()
+            return pymysql.connect(host='localhost',
+                                   user='root',
+                                   password=self.PASSWORD,
+                                   database=self.DB_NAME)
         else:
-            self.cnx = pymysql.connect(host='localhost',
-                                       user='root',
-                                       password=self.PASSWORD)
-            self.cursor = self.cnx.cursor()
+            return pymysql.connect(host='localhost',
+                                   user='root',
+                                   password=self.PASSWORD)
 
     def create_database(self):
         self.cursor.execute("SHOW DATABASES")
         database_existed = self.cursor.fetchall()
-        if ('crypto_currencies',) in database_existed:
+        if (self.db_name, ) in database_existed:
             print ("Database already exist")
             self.cursor.execute(f"USE {self.db_name}")
         else:
             self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.DB_NAME}")
-            self.cnx.commit()
+            self.connection.commit()
             print("Database created")
             self.cursor.execute(f"USE {self.DB_NAME}")
 
     def create_coins_table(self):
+        # if
         query = """CREATE TABLE IF NOT EXISTS coins
-                    (ID int NOT NULL PRIMARY KEY, coin varchar(45), price varchar(45),
+                    (ID int NOT NULL PRIMARY KEY, coin_name varchar(45), price varchar(45),
                      market_cap varchar(45), coin_url varchar(45))"""
         self.cursor.execute(query)
-        self.cnx.commit()
+        self.connection.commit()
         print("coins table created")
-        self.update_coins_list()
         
     def create_history_table(self):
         query = """CREATE TABLE IF NOT EXISTS history
@@ -73,41 +72,45 @@ class Database:
                      market_cap varchar(45), volume_of_flow varchar(45),
                      FOREIGN KEY (ID) REFERENCES coins(ID))"""
         self.cursor.execute(query)
-        self.cnx.commit()
+        self.connection.commit()
         print("history table created")
 
     def append_rows_to_coins(self, data):
         """
-        
+        :data: DataFrame of coins
         """
-        self.cursor.execute("SELECT coin From coins")
-        coins = [coin[0] for coin in self.cursor.fetchall()]
-        print(coins)
+        # self.cursor.execute("SELECT coin From coins")
+        # coins = [coin[0] for coin in self.cursor.fetchall()]
+        # print(coins)
         # data['coin_name'] = data['coin_name'].str.replace(' ', '-')
-        # print(data)
-        index_of_existing_coins = data.coin_name.isin(coins)
+        print(f"data:\n{data}", end='\n\n')
+        query = "SELECT coin_name From coins"
+        existing_coins = pd.read_sql(query, self.connection).coin_name
+
+        print(f"coins already in table: \n{existing_coins}", end='\n\n')
+
+        index_of_existing_coins = data.coin_name.isin(existing_coins)
+
         # pd.Series([coin not in self.coins for coin in data['coin_name']])
-        print(index_of_existing_coins)
+        print(f"existing coins: {data.loc[index_of_existing_coins, :]}")
         data_to_append = data[~index_of_existing_coins]
         data_to_update = data[index_of_existing_coins]
         
-        query = """INSERT INTO coins (ID, coin, price, market_cap, coin_url)
+        query = """INSERT INTO coins (ID, coin_name, price, market_cap, coin_url)
                     VALUES (%s, %s, %s, %s, %s)"""
         data = data_to_append.values.tolist()
         self.cursor.executemany(query, data)
-        self.cnx.commit()
+        self.connection.commit()
         print (f"{len(data_to_append)} rows were successfully uploaded")
         
         query = """UPDATE coins
                     SET price = %(price)s, market_cap = %(market_cap)s
-                    WHERE coin = %(coin_name)s"""
+                    WHERE coin_name = %(coin_name)s"""
         data = data_to_update.to_dict('records')
-        print(data)
+        print(f"coins to update:\n{data}")
         self.cursor.executemany(query, data)
-        self.cnx.commit()
-        # # except:
-        # #     pass
-        # self.update_coins_list()
+        self.connection.commit()
+        print(f"{len(data_to_update)} rows were successfully updated")
 
     def append_rows_to_history(self, data):
         data.fillna(method='ffill', inplace=True)
@@ -116,7 +119,7 @@ class Database:
                    VALUES (%(coin_id)s, %(price)s, %(market_cap)s, %(total_volume)s, %(snapped_at)s)"""
         try:
             self.cursor.executemany(query, data)
-            self.cnx.commit()
+            self.connection.commit()
             print (f"{len(data)} rows were successfully uploaded to the history table")
         except:
             print ("appending to history didn't successed")        
@@ -132,15 +135,15 @@ class Database:
     
     def close_connection(self):
         self.cursor.close()
-        self.cnx.close()
+        self.connection.close()
         print ("connection closed")
     
 # def main(df, df_hist):
-#     cnx, cursor = init()
+#     connection, cursor = init()
 #     df_hist.fillna(method='ffill', inplace=True)
-#     append_rows_to_coins(cnx, cursor, df)
-#     append_rows_to_history(cnx, cursor, df_hist)
-#     close_connection(cnx, cursor)
+#     append_rows_to_coins(connection, cursor, df)
+#     append_rows_to_history(connection, cursor, df_hist)
+#     close_connection(connection, cursor)
 
 
 # if __name__=='__main__':
@@ -148,3 +151,4 @@ class Database:
     # df, df_hist = CoinGecko.main()
     # df_hist.fillna(method='ffill', inplace=True)
 
+db = Database()
