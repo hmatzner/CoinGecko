a1ecd1f10d56c9fb5e17c41acd79d3a1cee6e58b
Hernan Matzner <hernoo.m@gmail.com>
2022-08-17 19:09:16 +0300

    docstring improvements

--- coingecko.py
+++ coingecko.py
@@ -1,18 +1,24 @@
 import webscraper
-from database_object2 import Database
+from database import Database
 import API
 import logging
 import sys
 
 
 def set_logger(name):
+    """
+    Performs the logging of a file given
+    @param name: name of the log file
+    @return: logger object
+    """
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter(
-        '%(asctime)s-%(levelname)s-FILE:%(filename)s-FUNC:%(funcName)s-LINE:%(lineno)d-%(message)s')
+        '%(asctime)s-%(levelname)s-FILE:%(filename)s-FUNC:%(funcName)s'
+        '-LINE:%(lineno)d-%(message)s')
 
-    file_handler = logging.FileHandler(name+'.log')
+    file_handler = logging.FileHandler(name + '.log')
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
@@ -26,6 +32,9 @@ def set_logger(name):
 
 
 def main():
+    """
+    Main function of the module that calls the webscraper, API and database files
+    """
     # df_coins, df_historical, df_wallets, df_distinct_wallets = webscraper.main()
     # Database(*webscraper.main(), logger=set_logger('database'))
     print(API.main(logger_input=set_logger('API')))
--- database.py
+++ database.py
@@ -7,14 +7,19 @@ class Database:
 
     def __init__(self, coins=None, hist=None, wallets=None, wallets_names=None, db_name=DB_NAME, logger=None):
         """
-        Initializes by creating the database and the tables
-        If given dataframes it will insert them to the relevant tables
+        Initializes the class by creating the database and the tables
+        @param coins: coins dataframe
+        @param hist: historical data dataframe
+        @param wallets: connection between wallets and coins dataframe
+        @param wallets_names: unique wallets dataframe
+        @param db_name: name of the database
+        @param logger: logger object
         """
         try:
             with open('.gitignore_folder/password.txt') as f:
                 self.PASSWORD = f.read()
         except FileNotFoundError:
-            self.PASSWORD = input("provide password for mySQL server: ")
+            self.PASSWORD = input("Provide password for MySQL server: ")
 
         self.db_name = db_name
         self.logger = logger
@@ -29,22 +34,21 @@ class Database:
         if 'coins' not in tables:
             self.create_coins_table()
         else:
-            self.logger.info("coins table was already existed")
+            self.logger.info("Coins table already existed")
         if 'history' not in tables:
             self.create_history_table()
         else:
-            self.logger.info("history table was already existed")
+            self.logger.info("History table already existed")
         if 'wallets' not in tables:
             self.create_wallets_table()
         else:
-            self.logger.info("wallets table was already existed")
+            self.logger.info("Wallets table already existed")
         if 'wallets_names' not in tables:
             self.create_wallets_names_table()
         else:
-            self.logger.info("wallets_names table was already existed")
+            self.logger.info("Wallets names table already existed")
 
         if coins is not None:
-            print(f"(database)coins: \n{type(coins)}")
             self.append_rows_to_coins(coins)
         if hist is not None:
             self.append_rows_to_history(hist)
@@ -56,7 +60,7 @@ class Database:
     def create_connection(self, use_db=True):
         """
         Creates and returns a connection
-        @param usd_db: if True uses {DB_NAME} as the database in the connection, otherwise doe'snt use database
+        @param use_db: if True uses the db_name as the database in the connection, otherwise it doesn't use one
         """
         if use_db:
             return pymysql.connect(host='localhost',
@@ -70,7 +74,7 @@ class Database:
 
     def create_database(self):
         """
-        Creates a database named {db_name}
+        Creates a database named [db_name]
         """
         self.cursor.execute("SHOW DATABASES")
         database_existed = self.cursor.fetchall()
@@ -85,7 +89,7 @@ class Database:
 
     def create_coins_table(self):
         """
-        Creates the table 'coins' in the database
+        Creates the 'coins' table in the database
         """
         query = """CREATE TABLE IF NOT EXISTS coins
                     (ID int NOT NULL PRIMARY KEY, coin_name varchar(45), price varchar(45),
@@ -96,7 +100,7 @@ class Database:
 
     def create_history_table(self):
         """
-        Creates a table called 'history' in the database
+        Creates the 'history' table in the database
         """
         query = """CREATE TABLE IF NOT EXISTS history
                     (ID int, date varchar(45), price varchar(45),
@@ -108,7 +112,7 @@ class Database:
 
     def create_wallets_table(self):
         """
-        Creates a table called 'wallets' in the database
+        Creates the 'wallets' table in the database
         """
         query = """CREATE TABLE wallets
                     (coin_id int, wallet_id int)"""
@@ -118,7 +122,7 @@ class Database:
 
     def create_wallets_names_table(self):
         """
-        Creates a table called 'wallets_names' in the database
+        Creates the 'wallets_names' table in the database
         """
         query = """CREATE TABLE wallets_names
                     (wallet_id int NOT NULL PRIMARY KEY, wallet_name varchar(45))"""
@@ -128,9 +132,9 @@ class Database:
 
     def append_rows_to_coins(self, data):
         """
-        :data: DataFrame of coins
-        Seperates new data and data that is already in the coins table,
-        Inserts the new data and updates the existings
+        Separates new data from other that already existed in the coins table
+        Inserts the new rows and updates the existing ones
+        @param data: coins dataframe
         """
         query = "SELECT coin_name From coins"
         existing_coins = pd.read_sql(query, self.connection).coin_name
@@ -142,6 +146,7 @@ class Database:
         query = """INSERT INTO coins (ID, coin_name, price, market_cap, coin_url)
                     VALUES (%s, %s, %s, %s, %s)"""
         data = data_to_append.values.tolist()
+
         try:
             self.cursor.executemany(query, data)
             self.connection.commit()
@@ -153,6 +158,7 @@ class Database:
                     SET price = %(price)s, market_cap = %(market_cap)s
                     WHERE coin_name = %(coin_name)s"""
         data = data_to_update.to_dict('records')
+
         try:
             self.cursor.executemany(query, data)
             self.connection.commit()
@@ -160,9 +166,9 @@ class Database:
         except Exception as e:
             self.logger.info(e)
 
-
     def append_rows_to_history(self, data):
         """
+
         Gets the data as a Pandas dataframe with columns:
         ['coin_id', 'total_volume', 'price', 'market_cap', 'snapped_at'].
         Inserts it into the 'history' table
